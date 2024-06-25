9157fcc750507b314c563a1f531a41c079faf68b
Tom Johnson <trjohnson3@gmail.com>
2024-06-25 13:49:43 -0500

    add props and css to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 
 
-function Header() {
-  return (
-    <header>
-        <div className="container">
-            <h2>Feedback UI</h2>
-        </div>
-    </header>
-  )
+function Header({ text, bgColor, textColor }) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    };
+
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6895'
+};
+
+export default Header;
